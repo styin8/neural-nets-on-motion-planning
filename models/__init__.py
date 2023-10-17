262f49581fb1a7f311515d59a78068d55ffd02cb
1st <531535977@qq.com>
2023-10-17 20:41:41 +0800

    cfn model..

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 import importlib
 from models.base_model import BaseModel
+import torch.nn as nn
 
 
 def find_model_using_name(model_name):
@@ -9,12 +10,11 @@ def find_model_using_name(model_name):
     model_filename = "models." + model_name + "_model"
     modellib = importlib.import_module(model_filename)
     model = None
-    target_model_name = model_name.replace("_","")+"model"
     for name, cls in modellib.__dict__.items():
-        if name.lower() == target_model_name.lower() and issubclass(cls, BaseModel):
+        if name.lower() == model_name.lower() and issubclass(cls, nn.Module):
             model = cls
     if model is None:
-        print(f"In {model_name}.py, there are no subclasses of BaseModel that match the {model_name}")
+        print(f"In {model_name}.py, there are no subclasses of nn.Module that match the {model_name}!")
     return model
     
 
---
+++ models/cfn_model.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+import torch
+
+class CFN(nn.Module):
+    def __init__(self, opt):
+        pass
+
+
+    def forward(self, x):
+        pass
+
+
+    def init_weights(self):
+        pass
